4e6873d3bcd246c3982e0e3dfeb154e9219fe900
Hantian Zhang <raisetoothless@gmail.com>
2026-05-20 00:34:02 -1000

    Add learningNote.md

--- learnFrontEnd2/src/App.jsx
+++ learnFrontEnd2/src/App.jsx
@@ -5,12 +5,13 @@ function App() {
   const movieNumber = 1;
 
   return (
-    <>//One way to do conditional rendering
+    <>{/*One way to do conditional rendering*/}
       {/* {movieNumber === 1 ? (
         <MovieCard movie = {{title: "Tim's Film", release_date: "2026-01-01"}} />
       ) : (
         <MovieCard movie = {{title: "Joe's Film 2", release_date: "2025-01-01"}} />
       )} */}
+      {/*second way to do conditional rendering*/}
       {movieNumber === 1 && <MovieCard movie = {{title: "Tim's Film", release_date: "2026-01-01"}} />}
       {movieNumber === 2 && <MovieCard movie = {{title: "Joe's Film 2", release_date: "2025-01-01"}} />}
     </>
---
+++ learningNote.md
@@ -0,0 +1,15 @@
+# Learn Front End 2
+
+## JSX
+JSX = JavaScript + HTML
+Makes UI easier to write and read
+
+## Components
+A component is just a JavaScript function that returns JSX.
+
+## Props
+Props means properties.
+Props allow us to pass data into components.
+
+## Conditional Rendering
+Conditional rendering shows different UI based on conditions.
