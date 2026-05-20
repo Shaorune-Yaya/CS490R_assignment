6209193b694d5a461e7deac69b7fdacf7817895a
Hantian Zhang <raisetoothless@gmail.com>
2026-05-20 00:23:14 -1000

    second way to do conditional rending

--- learnFrontEnd2/src/App.jsx
+++ learnFrontEnd2/src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import MovieCard from './components/MovieCard';
 
 function App() {
-  const movieNumber = 2;
+  const movieNumber = 1;
 
   return (
-    <>
-      {movieNumber === 1 ? (
+    <>//One way to do conditional rendering
+      {/* {movieNumber === 1 ? (
         <MovieCard movie = {{title: "Tim's Film", release_date: "2026-01-01"}} />
       ) : (
-      <MovieCard movie = {{title: "Joe's Film 2", release_date: "2025-01-01"}} />
-      )}
+        <MovieCard movie = {{title: "Joe's Film 2", release_date: "2025-01-01"}} />
+      )} */}
+      {movieNumber === 1 && <MovieCard movie = {{title: "Tim's Film", release_date: "2026-01-01"}} />}
+      {movieNumber === 2 && <MovieCard movie = {{title: "Joe's Film 2", release_date: "2025-01-01"}} />}
     </>
   );
 }
