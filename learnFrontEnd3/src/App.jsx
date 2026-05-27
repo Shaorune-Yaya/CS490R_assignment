1c7b753e6f764aa78d67519b2405118551a61753
Hantian Zhang <raisetoothless@gmail.com>
2026-05-26 23:32:21 -1000

    add search bar

--- learnFrontEnd3/src/App.jsx
+++ learnFrontEnd3/src/App.jsx
@@ -1,21 +1,11 @@
 import './App.css'
-import MovieCard from './components/MovieCard';
 import Home from './pages/Home';
 
 function App() {
-  const movieNumber = 1;
-
   return (
-    <>{/*One way to do conditional rendering*/}
-      {/* {movieNumber === 1 ? (
-        <MovieCard movie = {{title: "Tim's Film", release_date: "2026-01-01"}} />
-      ) : (
-        <MovieCard movie = {{title: "Joe's Film 2", release_date: "2025-01-01"}} />
-      )} */}
-      {/*second way to do conditional rendering*/}
-      <home />
-      {/* {movieNumber === 1 && <MovieCard movie = {{title: "Tim's Film", release_date: "2026-01-01"}} />}} */}
-      {/*{movieNumber === 2 && <MovieCard movie = {{title: "Joe's Film 2", release_date: "2025-01-01"}} />}*/}
+    <>
+      <Home />
+
     </>
   );
 }
