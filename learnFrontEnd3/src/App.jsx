1c0e933071e0f69e7d5e324cf9a3e868e3d8b4e6
Hantian Zhang <raisetoothless@gmail.com>
2026-05-26 23:09:33 -1000

    create learnFrontEnd3

--- learnFrontEnd3/src/App.jsx
+++ learnFrontEnd3/src/App.jsx
@@ -13,8 +13,9 @@ function App() {
         <MovieCard movie = {{title: "Joe's Film 2", release_date: "2025-01-01"}} />
       )} */}
       {/*second way to do conditional rendering*/}
-      {movieNumber === 1 && <MovieCard movie = {{title: "Tim's Film", release_date: "2026-01-01"}} />}
-      {movieNumber === 2 && <MovieCard movie = {{title: "Joe's Film 2", release_date: "2025-01-01"}} />}
+      <home />
+      {/* {movieNumber === 1 && <MovieCard movie = {{title: "Tim's Film", release_date: "2026-01-01"}} />}} */}
+      {/*{movieNumber === 2 && <MovieCard movie = {{title: "Joe's Film 2", release_date: "2025-01-01"}} />}*/}
     </>
   );
 }
