bf76cee5348a90f53154c60fafb818264d2fb7e1
Hantian Zhang <raisetoothless@gmail.com>
2026-06-03 01:11:37 -1000

    add api

---
+++ learnFrontEnd4/src/services/api.jsx
@@ -0,0 +1,14 @@
+const API_KEY = "7b80fd0a3af3d7b9379358bda855ebb2"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopulerMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+}
