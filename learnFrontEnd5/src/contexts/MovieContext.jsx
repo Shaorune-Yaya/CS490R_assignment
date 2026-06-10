c5771a864bf4a06f4d6802eccf141be8732f69a7
Hantian Zhang <raisetoothless@gmail.com>
2026-06-09 22:03:56 -1000

    update contexts

---
+++ learnFrontEnd5/src/contexts/MovieContext.jsx
@@ -0,0 +1,42 @@
+import {createContext, useState, useContext, useEffect} from "react";
+
+const MovieContext = createContext();
+
+export const useMovieContext = () => useContext(MovieContext);
+
+export const MovieProvider = ({children}) => {
+    const [favorites, setFavorites] = useState([]);
+
+    useEffect(() => {
+        const storedFavs = localStorage.getItem("favorites");
+
+        if(storedFavs) setFavorites(JSON.parse(storedFavs)) [1, 2, 3];
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+    }, [favorites]);
+
+    const addFavorite = (movie) => {
+        setFavorites(prev => [...prev, movie]);
+    }
+
+    const removeFromFavorites = (movieId) => {
+        setFavorites(prev => prev.filter(movie => movie.id !== movieId));
+    }
+
+    const isFavorite = (movieId) => {
+        return favorites.some(movie => movie.id === movieId);
+    }
+
+    const value = {
+        favorites,
+        addFavorite,
+        removeFromFavorites,
+        isFavorite
+    };
+
+    return <MovieContext.Provider value={value}>
+        {children}
+    </MovieContext.Provider>
+};
